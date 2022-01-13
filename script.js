aa642d38615fb7393f6a02d66468f204f47b9cdc
Saeid Gholami <saeidxgholami@gmail.com>
2022-01-13 20:49:06 +0330

    Use font

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const COLORS = [
+    { bg: "#f1faee", outline: "#f2cc8f", hover: "#fca311", header: "#457b9d", link: "#1d3557" },
+
+]
+
+let posts = document.getElementsByClassName("posts")[0];
+
+let post1 = posts.children[0];
+// post1.style.background = COLORS[0];
+
