246d2526b9411679bfa5288466113a82e891a557
Saeed Gholami <saeed.ghollami@gmail.com>
2024-10-15 23:25:44 +0330

    Hamburger menu for mobile screen.

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+// toggle hamberguer menu
+const ham = document.querySelector(".ham");
+const leftMenu = document.querySelector(".left-menu ul");
+const hamIcon = document.querySelector(".ham i");
 
+ham.addEventListener("click", () => {
+  leftMenu.classList.toggle("active");
+  if (leftMenu.classList.contains("active")) {
+    hamIcon.classList.remove("rotate-out");
+    hamIcon.classList.add("rotate-in"); // Animate to cross (X)
+    setTimeout(() => {
+      hamIcon.classList.remove("fa-bars");
+      hamIcon.classList.add("fa-xmark");
+      hamIcon.classList.remove("rotate-in"); // Animate to cross (X)
+    }, 400);
+  } else {
+    hamIcon.classList.add("rotate-in"); // Animate to cross (X)
+    setTimeout(() => {
+      hamIcon.classList.remove("fa-xmark");
+      hamIcon.classList.add("fa-bars");
+      hamIcon.classList.remove("rotate-in"); // Animate to cross (X)
+      hamIcon.classList.add("rotate-out"); // Animate to cross (X)
+    }, 400);
+  }
+});
 
+// Close the menu when clicking outside the menu or hamburger
+document.addEventListener("click", (event) => {
+  // Check if the clicked target is not the menu or hamburger
+  if (!leftMenu.contains(event.target) && !ham.contains(event.target)) {
+    leftMenu.classList.remove("active"); // Close the menu
+    // remove x icon if after user clicked anywhere of page.
+    if (hamIcon.classList.contains("fa-xmark")) {
+      hamIcon.classList.remove("fa-xmark");
+      hamIcon.classList.add("fa-bars");
+    }
+  }
+});
 
-// how many page I've been red form 
-let pages = document.getElementById("value").textContent;
-let red_page = parseInt(pages.split(" ")[1]);
-let total_pages = parseInt(pages.split(" ")[3]);
-
-// Calculate done percteged 
-let done_percentage = Math.round((100 / total_pages) * red_page);
-let done_percentage_format = `${done_percentage}%`;
-let progress = document.getElementById("progress");
-progress.innerHTML = done_percentage_format;
-// Update width of the progress in progressbar
-progress.style.width = done_percentage_format;
